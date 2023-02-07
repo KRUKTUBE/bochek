6bd40937b544c0a1c731eaf5736ed7e606b70930
KRUKTUBE <114464432+KRUKTUBE@users.noreply.github.com>
2023-02-07 18:53:33 +0700

    Delete i1.py

--- i1.py
+++
@@ -1,9 +0,0 @@
-print("Войдите в профиль")
-while True:
-    l=input("Введите логин: ")
-    p=input("Введите пароль: ")
-    if l == "moshka" and p == "bobka":
-        print("Успешный вход")
-        break
-    else:
-        print("Не верно! Введите ещё раз")
